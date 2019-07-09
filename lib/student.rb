bc5c1e0fd22972bbba165e0480c75e7b8ade6783
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-07-09 20:29:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      student = self.new(name, location)
+      student 
       
       
       binding.pry
